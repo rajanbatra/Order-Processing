93ff85464e52d512a6d19efdd26dab2bf501a6eb
Rajan <batra.rajan97@gmail.com>
2021-03-02 11:32:33 -0800

    Add unit tests

--- src/test/java/com/orderprocessing/OrderprocessingApplicationTests.java
+++ src/test/java/com/orderprocessing/OrderprocessingApplicationTests.java
@@ -5,13 +5,10 @@ import com.orderprocessing.services.BulkOrderService;
 import com.orderprocessing.services.OrderService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 import java.util.Arrays;
